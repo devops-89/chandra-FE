675305ee6729f09d6e92711020fbaca14dc09689
shivam-495 <shivam.kumar@digixito.org>
2026-06-19 13:44:32 +0530

    contact page navigation fix

--- frontend/src/components/contactSection/ContactSection.tsx
+++ frontend/src/components/contactSection/ContactSection.tsx
@@ -4,7 +4,6 @@ import ContactInfoCard from './ContactInfoCard';
 export default function ContactSection() {
   return (
     <section 
-    id='contact'
     className="max-w-7xl mx-auto px-6 pb-24">
       <div className="grid lg:grid-cols-12 gap-8">
         <div className="lg:col-span-8">
