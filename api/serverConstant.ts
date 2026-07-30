41ee79a586bd37638a955624ac8c83ae204a4a40
Yashwin <yashwin.sharma@digixito.org>
2026-07-30 10:56:25 +0530

    initial commit

--- api/serverConstant.ts
+++ api/serverConstant.ts
@@ -1,4 +1,4 @@
 export const SERVER_ENDPOINTS = {
-  AUTH_BASEURL: 'http://13.60.216.166/api',
-  USER_BASEURL: 'http://13.60.216.166/api',
+  AUTH_BASEURL: 'http://192.168.1.38:8000/api',
+  USER_BASEURL: 'http://192.168.1.38:8001/api',
 };
