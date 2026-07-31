bfb06cb6908d0201f09be866f9a566e18977e656
Akash God <111779287+Akash-dev-lab@users.noreply.github.com>
2026-07-31 17:43:14 +0530

    commit

--- api/serverConstant.ts
+++ api/serverConstant.ts
@@ -1,4 +1,4 @@
 export const SERVER_ENDPOINTS = {
-  AUTH_BASEURL: 'http://192.168.1.7:8000/api',
-  USER_BASEURL: 'http://192.168.1.7:8001/api',
+  AUTH_BASEURL: 'http://192.168.31.34:8000/api',
+  USER_BASEURL: 'http://192.168.31.34:8001/api',
 };
